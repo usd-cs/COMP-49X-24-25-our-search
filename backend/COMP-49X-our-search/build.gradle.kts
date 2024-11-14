ebc6ec9249e182b2ba5dd6a6daf9952fedfedc8e
natjungquist <njungquist@sandiego.edu>
2024-11-14 15:32:12 -0800

    initialized backend boilerplate code

---
+++ backend/COMP-49X-our-search/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "our-search"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/COMP-49X-our-search/src/test/java/our_search/COMP_49X_our_search/Comp49XOurSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package our_search.COMP_49X_our_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp49XOurSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
