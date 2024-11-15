0b70a4f7f1c32262367fccb1311a7f0642c35e07
natjungquist <njungquist@sandiego.edu>
2024-11-14 16:47:45 -0800

    added .properties file to tests directory for github actions

--- backend/src/test/java/COMP_49X_our_search/backend/BackendApplicationTests.java
+++ backend/src/test/java/COMP_49X_our_search/backend/BackendApplicationTests.java
@@ -2,7 +2,9 @@ package COMP_49X_our_search.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
