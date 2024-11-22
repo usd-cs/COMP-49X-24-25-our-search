1d3bd443e4da10bcec2a2c716367b1ccd6679dc5
aescudero <aescudero@sandiego.edu>
2024-11-22 00:04:02 -0800

    Added missing tests

---
+++ backend/src/test/java/COMP_49X_our_search/backend/database/DepartmentServiceTest.java
@@ -0,0 +1,38 @@
+package COMP_49X_our_search.backend.database;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import COMP_49X_our_search.backend.database.entities.Department;
+import COMP_49X_our_search.backend.database.repositories.DepartmentRepository;
+import COMP_49X_our_search.backend.database.services.DepartmentService;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class DepartmentServiceTest {
+
+  @Autowired private DepartmentService departmentService;
+
+  @MockBean private DepartmentRepository departmentRepository;
+
+  @Test
+  void testGetAllDepartments() {
+    Department engineeringDepartment = new Department("Engineering");
+    Department lifeSciencesDepartment = new Department("Life Sciences");
+
+    Mockito.when(departmentRepository.findAll())
+        .thenReturn(List.of(engineeringDepartment, lifeSciencesDepartment));
+
+    List<Department> departments = departmentService.getAllDepartments();
+
+    assertEquals(2, departments.size());
+    assertTrue(departments.containsAll(List.of(engineeringDepartment, lifeSciencesDepartment)));
+  }
+}
---
+++ backend/src/test/java/COMP_49X_our_search/backend/database/MajorServiceTest.java
@@ -0,0 +1,39 @@
+package COMP_49X_our_search.backend.database;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import COMP_49X_our_search.backend.database.entities.Department;
+import COMP_49X_our_search.backend.database.entities.Major;
+import COMP_49X_our_search.backend.database.repositories.MajorRepository;
+import COMP_49X_our_search.backend.database.services.MajorService;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class MajorServiceTest {
+
+  @Autowired private MajorService majorService;
+
+  @MockBean private MajorRepository majorRepository;
+
+  @Test
+  void testGetAllMajors() {
+    Department engineeringDepartment = new Department("Engineering");
+    Major csMajor = new Major("Computer Science", engineeringDepartment, null, null);
+    Major mathMajor = new Major("Mathematics", engineeringDepartment, null, null);
+
+    Mockito.when(majorRepository.findAll()).thenReturn((List.of(csMajor, mathMajor)));
+
+    List<Major> majors = majorService.getAllMajors();
+
+    assertEquals(2, majors.size());
+    assertTrue(majors.containsAll(List.of(csMajor, mathMajor)));
+  }
+}
