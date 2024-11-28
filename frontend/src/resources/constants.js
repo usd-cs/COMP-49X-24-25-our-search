bf0d1803fb6faa8d9e9b08c8fa9d3877294dfb10
natjungquist <njungquist@sandiego.edu>
2024-11-27 20:14:41 -0600

    implemented TitleButton component and made a constants.js file to store things like the app title

---
+++ frontend/src/resources/constants.js
@@ -0,0 +1,7 @@
+export const appTitle = "OUR SEARCH";
+
+export const errorLoadingPostingsMessage = "Error loading site data";
+export const noPostsMessage = "None available";
+
+export const fetchProjectsUrl = "http://localhost:8080/projects";
+export const fetchStudentsUrl = "http://localhost:8080/students";
