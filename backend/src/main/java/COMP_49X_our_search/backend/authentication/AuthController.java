b84062a27ee05f93e49e531106fca680cc8d8093
natjungquist <njungquist@sandiego.edu>
2025-02-14 13:30:38 -0800

    controller and service implemented to return info on user's authentication status with oauth2

---
+++ backend/src/main/java/COMP_49X_our_search/backend/authentication/AuthController.java
@@ -0,0 +1,58 @@
+/**
+ * This controller handles an authentication-related endpoint for the application.
+ * The `/check-auth` endpoint checks whether a user is authenticated or not and their user role
+ * and returns a JSON response accordingly.
+ *
+ * @author Natalie Jungquist
+ */
+
+// TODO : NOTE THIS IS CURRENTLY A CLASS STUB. ITS TESTS AND FUNCTIONS HAVE NOT BEEN IMPLEMENTED
+
+package COMP_49X_our_search.backend.authentication;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PublicKey;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+
+    private final OAuthChecker OAuthChecker;
+
+    public AuthController(OAuthChecker OAuthChecker) {
+        this.OAuthChecker = OAuthChecker;
+    }
+
+    @GetMapping("/check-auth")
+    public ResponseEntity<Map<String, String>> checkAuthStatus() {
+
+        Map<String, String> response = OAuthChecker.getDefaultResponse();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (OAuthChecker.isAuthenticated(authentication)) {
+            response.put("isAuthenticated", "true");
+
+            String email = OAuthChecker.getAuthUserEmail(authentication);
+            System.out.println("email: " + email);
+
+            // TODO check db if they already exist based on the email. returning a default value for now
+            response.put("isStudent", "true");
+//            response.put("isFaculty", "true");
+//            response.put("isAdmin", "true");
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+    @RequestMapping("/invalid-email")
+    public String invalidEmail() {
+        return "forward:invalid-email.html";
+    }
+
+}
---
+++ backend/src/main/java/COMP_49X_our_search/backend/authentication/OAuthChecker.java
@@ -0,0 +1,46 @@
+/**
+ * Implementation responsible for handling authentication-related utility functions.
+ * Specifically for OAuth2 users.
+ *  - Check if a user is authenticated.
+ *  - Retrieve the authenticated user's email.
+ *  - Provide a default authentication response structure.
+ */
+
+package COMP_49X_our_search.backend.authentication;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class OAuthChecker implements AuthCheckerInterface {
+
+    @Override
+    public Map<String, String> getDefaultResponse() {
+        Map<String, String> response = new HashMap<>();
+        response.put("isAuthenticated", "false");
+        response.put("isStudent", "false");
+        response.put("isFaculty", "false");
+        response.put("isAdmin", "false");
+        return response;
+    }
+
+    @Override
+    public boolean isAuthenticated(Authentication authentication) {
+        return authentication instanceof OAuth2AuthenticationToken;
+    }
+
+    @Override
+    public String getAuthUserEmail(Authentication authentication) {
+        if (authentication instanceof OAuth2AuthenticationToken oauthToken) {
+            OAuth2User user = oauthToken.getPrincipal();
+            Map<String, Object> attributes = user.getAttributes();
+            return (attributes != null && attributes.containsKey("email")) ? attributes.get("email").toString() : "";
+        }
+        return "";
+    }
+}
