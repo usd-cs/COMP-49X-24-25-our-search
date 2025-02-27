3cc5acb42fa8902bb266de94b3bf732b05a101a2
aescudero <aescudero@sandiego.edu>
2025-02-26 23:34:17 -0800

    Updated file header for ModuleController.java that I missed

--- backend/src/main/java/COMP_49X_our_search/backend/core/ModuleController.java
+++ backend/src/main/java/COMP_49X_our_search/backend/core/ModuleController.java
@@ -1,5 +1,11 @@
 /**
- * Defines method for 
+ * Defines method contract for processing module configurations.
+ *
+ * This interface servers as an entry point for handling different types of
+ * complex requests that are module-specific, such as fetching data, as well as
+ * managing profiles. Implementations of this interface should process the
+ * given {@link proto.core.Core.ModuleConfig} and return an appropriate
+ * {@link proto.core.Core.ModuleResponse}
  *
  * @author Augusto Escudero
  */
