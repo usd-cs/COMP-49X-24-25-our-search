8b2afd4b53bf1f3e803611147af219ca3b103a88
natjungquist <njungquist@sandiego.edu>
2025-02-28 12:41:48 -0800

    created new securityconstants class to define constants shared by all classes related to security

---
+++ backend/src/main/java/COMP_49X_our_search/backend/security/SecurityConstants.java
@@ -0,0 +1,23 @@
+/**
+ * This class defines constants that are shared between the classes in the security package,
+ * as well as their respective test cases.
+ * //
+ * All routes and URLs used in the security configuration and authentication flows
+ * are centralized here to ensure consistency and accessibility across both the
+ * application source code and test cases.
+ *
+ * @author Natalie Jungquist
+ */
+
+package COMP_49X_our_search.backend.security;
+
+public class SecurityConstants {
+
+    static final String FRONTEND_URL = "http://localhost:3000";
+    static final String ALLOWED_DOMAIN = "@sandiego.edu";
+    static final String INVALID_EMAIL_PATH = "/invalid-email";
+    static final String NO_PROFILE_PATH = "/ask-for-role";
+    static final String HAS_PROFILE_PATH = "/posts";
+    static final String GOOGLE_LOGOUT_URL = "https://accounts.google.com/logout";
+    static final String OAUTH_REDIRECT_ENDPOINT = "/login/oauth2/code/google"; // this is created by spring security/google
+}
