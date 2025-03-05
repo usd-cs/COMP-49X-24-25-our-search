1d44f43181a5d1c3144772d1210611ef98749053
aescudero <aescudero@sandiego.edu>
2025-03-04 20:36:31 -0800

    Fixed endpoints for editing students

--- backend/src/main/java/COMP_49X_our_search/backend/gateway/GatewayController.java
+++ backend/src/main/java/COMP_49X_our_search/backend/gateway/GatewayController.java
@@ -39,6 +39,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -288,7 +289,7 @@ public class GatewayController {
     }
   }
 
-  @PostMapping("/api/studentProfiles/edit")
+  @PutMapping("/api/studentProfiles/current")
   public ResponseEntity<StudentDTO> editStudentProfile(
       @RequestBody EditStudentRequestDTO requestBody) {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
