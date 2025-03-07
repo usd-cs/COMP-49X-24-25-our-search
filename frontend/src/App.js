d958c2154cf6ccefc3be5a99f3aa99f1bddf5112
natjungquist <njungquist@sandiego.edu>
2025-03-06 16:24:43 -0800

    fixed logout bug -it wasn't allowed due to CORS policies - google logout endpoing can only be requested by the backend, not directly from the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,10 +29,10 @@ import FacultyProfileEdit from './components/profiles/FacultyProfileEdit.js'
 import ResearchOpportunityForm from './components/ResearchOpportunityForm.js'
 
 function App () {
-  const [isAuthenticated, setisAuthenticated] = useState(true)
+  const [isAuthenticated, setisAuthenticated] = useState(false)
   const [isStudent, setIsStudent] = useState(false)
   const [isAdmin, setIsAdmin] = useState(false)
-  const [isFaculty, setIsFaculty] = useState(true)
+  const [isFaculty, setIsFaculty] = useState(false)
   const [error505, setError505] = useState(false)
   const [loading, setLoading] = useState(true) // Loading state is required to ensure that nothing loads until the call to the backend has returned a response.
 
@@ -87,14 +87,7 @@ function App () {
       setIsStudent(false)
       setIsFaculty(false)
       setIsAdmin(false)
-
-      await fetch(`${backendUrl}/logout`, {
-        credentials: 'include',
-        method: 'POST',
-        redirect: 'follow'
-      })
-
-      window.location.href = '/'
+      window.location.href = `${backendUrl}/logout`
     } catch (error) {
       console.error('Error logging out:', error)
     }
