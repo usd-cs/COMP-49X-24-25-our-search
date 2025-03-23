efa8719f81da93a6ac2021378d35a6ba73ea8525
aescudero <aescudero@sandiego.edu>
2025-03-23 14:54:26 -0700

    Finished implementation

--- backend/src/main/java/COMP_49X_our_search/backend/gateway/GatewayController.java
+++ backend/src/main/java/COMP_49X_our_search/backend/gateway/GatewayController.java
@@ -54,6 +54,7 @@ import proto.data.Entities.FacultyProto;
 import proto.data.Entities.ProjectProto;
 import proto.data.Entities.StudentProto;
 import proto.fetcher.FetcherModule.FetcherRequest;
+import proto.fetcher.FetcherModule.FetcherResponse;
 import proto.fetcher.FetcherModule.FilteredFetcher;
 import proto.fetcher.FetcherModule.FilteredType;
 import proto.profile.ProfileModule.CreateProfileRequest;
@@ -256,7 +257,7 @@ public class GatewayController {
     try {
       List<DepartmentDTO> departmentDTOs =
           departmentService.getAllDepartments().stream()
-              .map(department -> new DepartmentDTO(department.getId(), department.getName(), null))
+              .map(department -> new DepartmentDTO(department.getId(), department.getName(), null, null))
               .toList();
       return ResponseEntity.ok(departmentDTOs);
 
@@ -506,7 +507,7 @@ public class GatewayController {
                       new DepartmentDTO(
                           departmentService.getDepartmentByName(departmentName).get().getId(),
                           departmentName,
-                          null))
+                          null, null))
               .toList();
 
       FacultyProfileDTO facultyProfileDTO = new FacultyProfileDTO();
@@ -582,5 +583,23 @@ public class GatewayController {
       return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+  }
+
+  @GetMapping("/all-faculty")
+  public ResponseEntity<List<DepartmentDTO>> getAllFaculty() {
+    ModuleConfig moduleConfig = ModuleConfig.newBuilder()
+        .setFetcherRequest(FetcherRequest.newBuilder()
+            .setFilteredFetcher(FilteredFetcher.newBuilder()
+                .setFilteredType(FilteredType.FILTERED_TYPE_FACULTY)
+            )
+        ).build();
+
+    ModuleResponse response = moduleInvoker.processConfig(moduleConfig);
+    FetcherResponse fetcherResponse = response.getFetcherResponse();
+
+    return ResponseEntity.ok(
+        fetcherResponse.getDepartmentHierarchy().getDepartmentsList().stream()
+            .map(ProjectHierarchyConverter::protoDepartmentWithFacultyToDto).toList()
+    );
   }
 }
