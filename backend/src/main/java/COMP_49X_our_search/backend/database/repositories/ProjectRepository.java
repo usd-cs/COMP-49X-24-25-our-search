10982de719fbffaff12c1daac5788b5f07ea345a
aescudero <aescudero@sandiego.edu>
2025-03-24 22:03:59 -0700

    Removed unused import

--- backend/src/main/java/COMP_49X_our_search/backend/database/repositories/ProjectRepository.java
+++ backend/src/main/java/COMP_49X_our_search/backend/database/repositories/ProjectRepository.java
@@ -10,7 +10,6 @@ package COMP_49X_our_search.backend.database.repositories;
 
 import COMP_49X_our_search.backend.database.entities.Project;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
