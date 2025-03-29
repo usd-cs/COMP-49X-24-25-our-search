552831da58db2f3d79b1accbac3203f9ea3ebc49
natjungquist <njungquist@sandiego.edu>
2025-03-28 17:30:11 -0700

    fixed name field in add requests

--- frontend/src/__tests__/admin/add/handleAddPeriod.test.js
+++ frontend/src/__tests__/admin/add/handleAddPeriod.test.js
@@ -46,7 +46,7 @@ describe('handleAddPeriod', () => {
       credentials: 'include',
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ newPeriodName })
+      body: JSON.stringify({ name: newPeriodName })
     })
   })
 
