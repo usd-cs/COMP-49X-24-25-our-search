70336851f1c4b1fb1e49f4cd535c7e86fc471852
natjungquist <njungquist@sandiego.edu>
2025-03-30 14:17:30 -0700

    new constants for faq fetching defined

--- frontend/src/resources/constants.js
+++ frontend/src/resources/constants.js
@@ -12,3 +12,12 @@ export const backendUrl = 'http://localhost:8080'
 export const fetchProjectsUrl = backendUrl + '/all-projects'
 export const fetchStudentsUrl = backendUrl + '/all-students'
 export const fetchFacultyUrl = backendUrl + '/all-faculty'
+
+export const typeStudent = 'student'
+export const typeFaculty = 'faculty'
+export const typeAdmin = 'admin'
+
+export const fetchStudentFAQsUrl = backendUrl + '/all-student-faqs'
+export const fetchFacultyFAQsUrl = backendUrl + '/all-faculty-faqs'
+export const fetchAdminFAQsUrl = backendUrl + '/all-admin-faqs'
+export const FAQUrl = backendUrl + '/faq'
