3512e52b7d36ab0651b3418cdc143f9f456f7d03
natjungquist <njungquist@sandiego.edu>
2025-03-30 14:17:10 -0700

    wrote expected requests/responses in mockdata for edit, add, and delete specific FAQ and get all FAQs

--- frontend/src/resources/mockData.js
+++ frontend/src/resources/mockData.js
@@ -764,3 +764,69 @@ export const mockOneFaculty = {
     }
   ]
 }
+
+// GET /all-student-faqs
+export const getAllStudentFAQsResponse = [
+  {
+    id: 1,
+    question: 'How do I edit my profile?',
+    answer: 'Click top right button'
+  },
+  {
+    id: 2,
+    question: 'How do I find projects related to my major?',
+    answer: 'Open side bar and filter and do stuff. It is pretty cool what you can do wow. ' +
+    'This is a sample longer answer to see how it looks on the page. It just keeps going omg yep ' +
+    'there is just so much to say on this topic mhm. Do you understand? Hope it makes sense. ' +
+    'Contact OUR email address for further inquiries'
+  }
+]
+
+// GET /all-faculty-faqs
+export const getAllFacultyFAQsResponse = [
+  {
+    id: 1,
+    question: 'How do I edit my faculty stuff like projects yknow?',
+    answer: 'sample answer'
+  },
+  {
+    id: 2,
+    question: 'How do I find?',
+    answer: 'blah faculty'
+  }
+]
+
+// GET /all-admin-faqs
+export const getAllAdminFAQsResponse = [
+  {
+    id: 1,
+    question: 'How do I edit a student profile?',
+    answer: 'Click'
+  },
+  {
+    id: 2,
+    question: 'How do I delete a major?',
+    answer: 'Open things'
+  }
+]
+
+// PUT /faq
+export const putFaqExpectedRequest = {
+  id: 3,
+  type: 'student', //or 'faculty' or 'admin'
+  question: 'What do I do?',
+  answer: 'You find out.'
+}
+
+// DELETE /faq
+export const deleteFaqExpectedRequest = {
+  id: 3,
+  type: 'student', //or 'faculty' or 'admin'
+}
+
+// POST /faq
+export const postFaqExpectedRequest = {
+  type: 'student', //or 'faculty' or 'admin'
+  question: 'How do I do it?',
+  answer: 'With xyz steps.'
+}
