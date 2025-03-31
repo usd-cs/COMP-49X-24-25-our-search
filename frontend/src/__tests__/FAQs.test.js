753e69e6d5775d08b432d96fe7ce54d341354602
natjungquist <njungquist@sandiego.edu>
2025-03-30 17:13:22 -0700

    WIP tests for FAQs main page

--- frontend/src/__tests__/FAQs.test.js
+++ frontend/src/__tests__/FAQs.test.js
@@ -76,20 +76,175 @@ describe('FAQs', () => {
 
   })
 
-  describe('conditional rendering when it loads up', () => {
+  describe('conditional rendering', () => {
+
+    describe('for students', () => {
+      test('displays error message if initial fetching fails', async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+          ok: false,
+          statusText: 'Internal Server Error'
+        })
+    
+        renderWithTheme(<FAQs showingStudentFAQs />)
+    
+        await waitFor(() => {expect(screen.getByText(/Error loading/i)).toBeInTheDocument()
+        })
+      })
+      test('renders existing FAQs', async () => {
+        renderWithTheme(<FAQs showingStudentFAQs />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+        expect(screen.getByText(getAllStudentFAQsResponse[0].question)).toBeInTheDocument()
+        expect(screen.getByText(getAllStudentFAQsResponse[1].question)).toBeInTheDocument()
+      })
+      test('does not render existing FAQs for faculty or admin', async () => {
+        renderWithTheme(<FAQs showingStudentFAQs />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+        expect(screen.queryByText(getAllAdminFAQsResponse[0].question)).not.toBeInTheDocument()
+        expect(screen.queryByText(getAllFacultyFAQsResponse[0].question)).not.toBeInTheDocument()
+      })
+    })
+
+    describe('for faculty', () => {
+      test('displays error message if initial fetching fails', async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+          ok: false,
+          statusText: 'Internal Server Error'
+        })
+    
+        renderWithTheme(<FAQs showingFacultyFAQs />)
+    
+        await waitFor(() => {expect(screen.getByText(/Error loading/i)).toBeInTheDocument()
+        })
+      })
+      test('renders existing FAQs', async () => {
+        renderWithTheme(<FAQs showingFacultyFAQs />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+        expect(screen.getByText(getAllFacultyFAQsResponse[0].question)).toBeInTheDocument()
+        expect(screen.getByText(getAllFacultyFAQsResponse[1].question)).toBeInTheDocument()
+      })
+      test('does not render existing FAQs for student or admin', async () => {
+        renderWithTheme(<FAQs showingFacultyFAQs />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+        expect(screen.queryByText(getAllAdminFAQsResponse[0].question)).not.toBeInTheDocument()
+        expect(screen.queryByText(getAllStudentFAQsResponse[0].question)).not.toBeInTheDocument()
+      })
+    })
+
+    describe('for admin', () => {
+      test('displays error message if initial fetching fails', async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+          ok: false,
+          statusText: 'Internal Server Error'
+        })
+    
+        renderWithTheme(<FAQs showingAdminFAQs />)
+    
+        await waitFor(() => {expect(screen.getByText(/Error loading/i)).toBeInTheDocument()
+        })
+      })
+      test('renders existing FAQs', async () => {
+        renderWithTheme(<FAQs showingAdminFAQs />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+        expect(screen.getByText(getAllAdminFAQsResponse[0].question)).toBeInTheDocument()
+        expect(screen.getByText(getAllAdminFAQsResponse[1].question)).toBeInTheDocument()
+      })
+      test('does not render existing FAQs for faculty or student', async () => {
+        renderWithTheme(<FAQs showingAdminFAQs />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+        expect(screen.queryByText(getAllStudentFAQsResponse[0].question)).not.toBeInTheDocument()
+        expect(screen.queryByText(getAllFacultyFAQsResponse[0].question)).not.toBeInTheDocument()
+      })
+
+      describe('deleting', () => {
+        test('asks for confirmation before deleting', async () => {
+          renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+          await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+          const deleteButton = screen.getAllByTestId('delete-btn')[0]
+          fireEvent.click(deleteButton)
+  
+          expect(screen.getAllByText(/Are you sure you want to delete/i).length).toBeGreaterThan(0)
+  
+          const confirmDelete = screen.getAllByTestId('confirm')[0]
+          expect(confirmDelete).toBeInTheDocument()
+        })
+      })
+    
+      describe('editing', () => {
+        test('shows edit button; shows save button after clicking edit', async () => {
+          renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+          await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+          const editButton = screen.getAllByTestId('edit-btn')[0]
+          expect(editButton).toBeInTheDocument()
+          fireEvent.click(editButton)
+  
+          const saveButton = screen.getAllByTestId('save-btn')[0]
+          expect(saveButton).toBeInTheDocument()
+        })
+        test('shows new details after edited', async () => {
+          renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+          await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+          const editButton = screen.getAllByTestId('edit-btn')[0]
+          expect(editButton).toBeInTheDocument()
+          fireEvent.click(editButton)
+  
+          const questionInput = screen.getByDisplayValue(getAllAdminFAQsResponse[0].question)
+          const answerInput = screen.getByDisplayValue(getAllAdminFAQsResponse[0].answer)
+          const newQuestion = 'Mock new question'
+          fireEvent.change(questionInput, { target: { value: newQuestion } })
+          const newAnswer = 'Mock new answer here'
+          fireEvent.change(answerInput, { target: { value: newAnswer } })
+  
+          expect(questionInput.value).toBe(newQuestion)
+          expect(answerInput.value).toBe(newAnswer)
+        })
+        test('shows an option to cancel editing', async () => {
+          renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+          await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+          const editButton = screen.getAllByTestId('edit-btn')[0]
+          expect(editButton).toBeInTheDocument()
+          fireEvent.click(editButton)
+  
+          const cancelButton = screen.getAllByTestId('cancel-btn')[0]
+          expect(cancelButton).toBeInTheDocument()
+        })
+      })
+    
+      describe('adding', () => {
+        test('there is a place to add new', async () => {
+          renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+          await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+  
+          const newQuestionInput = screen.getByLabelText(/new question/i)
+          const newAnswerInput = screen.getByLabelText(/new answer/i)
+          const addButton = screen.getAllByTestId('add-btn')[0]
+  
+          expect(newQuestionInput).toBeInTheDocument()
+          expect(newAnswerInput).toBeInTheDocument()
+          expect(addButton).toBeInTheDocument()
+        })
+      })
+
+      test(' ', async () => {
+        renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+
+      })
+      test('  ', async () => {
+        renderWithTheme(<FAQs showingAdminFAQs isAdmin />)
+        await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument())
+       
+      })
+    })
 
   })
-
-  describe('deleting', () => {
-
-  })
-
-  describe('editing', () => {
-
-  })
-
-  describe('adding', () => {
-
-  })
-
 })
