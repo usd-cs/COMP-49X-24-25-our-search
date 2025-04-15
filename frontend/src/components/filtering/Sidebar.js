09666973554b45e813119f4d431ba05c6a02dc07
natjungquist <njungquist@sandiego.edu>
2025-04-15 15:57:00 -0700

    feat: sidebar loads based on postsview

--- frontend/src/components/filtering/Sidebar.js
+++ frontend/src/components/filtering/Sidebar.js
@@ -4,16 +4,123 @@
  * @author Natalie Jungquist <njungquist@sandiego.edu>
  */
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import {
   Box, Typography, Drawer,
-  List, ListItem, ListItemButton, ListItemIcon, ListItemText
+  Button
 } from '@mui/material'
-import SortRoundedIcon from '@mui/icons-material/SortRounded'
-import { CUSTOM_BG_COLOR } from '../../resources/constants'
+import { CUSTOM_BG_COLOR, CUSTOM_BUTTON_COLOR, viewFacultyFlag, viewMyProjectsFlag, viewProjectsFlag, viewStudentsFlag } from '../../resources/constants'
 
-function Sidebar ({ drawerWidth, iconColor, open, isAdmin }) {
-  const navigate = useNavigate()
+function Sidebar ({ drawerWidth, open, postsView, toggleDrawer }) {
+  // const navigate = useNavigate()
+
+  // move close filters to inside bar
+
+  if (!postsView) postsView = viewProjectsFlag
+
+  const renderButtons = () => {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
+        <Button
+          variant='outlined'
+      // onClick={()} navigate('/posts?umbrellatopics=topic1,topic2 & majors=major1,major2 blah')
+          sx={{
+            borderColor: CUSTOM_BUTTON_COLOR,
+            textTransform: 'none',
+            fontWeight: 'bold',
+            color: CUSTOM_BUTTON_COLOR,
+            borderRadius: '20px',
+            '&:hover': {
+              backgroundColor: `${CUSTOM_BUTTON_COLOR}80`
+            },
+            px: 2,
+            py: 1,
+            mt: 2
+          }}
+        >
+          Apply
+        </Button>
+        <Button
+          variant='outlined'
+      // onClick={()}
+          sx={{
+            borderColor: CUSTOM_BUTTON_COLOR,
+            textTransform: 'none',
+            fontWeight: 'bold',
+            color: CUSTOM_BUTTON_COLOR,
+            borderRadius: '20px',
+            '&:hover': {
+              backgroundColor: `${CUSTOM_BUTTON_COLOR}80`
+            },
+            px: 2,
+            py: 1,
+            mt: 2
+          }}
+        >
+          Reset
+        </Button>
+      </Box>
+    )
+  }
+
+  const renderCloseButton = () => {
+    return (
+      <Button
+        onClick={toggleDrawer}
+        sx={{
+          position: 'absolute',
+          top: 8,
+          right: 8,
+          minWidth: '40px',
+          width: '40px',
+          height: '40px',
+          borderRadius: '20px',
+          p: 0,
+          '&:hover': {
+            backgroundColor: 'rgba(0, 0, 0, 0.04)'
+          }
+        }}
+      >
+        X
+      </Button>
+    )
+  }
+
+  const renderDrawerContent = () => {
+    if (postsView === viewStudentsFlag) {
+      return (
+        <Box sx={{ width: drawerWidth, CUSTOM_BG_COLOR }} role='presentation'>
+          {renderCloseButton()}
+          <Typography variant='h6' sx={{ mt: 5 }}>
+            Filters will go here for students
+          </Typography>
+          {renderButtons()}
+
+        </Box>
+      )
+    } else if (postsView === viewProjectsFlag || postsView === viewMyProjectsFlag) {
+      return (
+        <Box sx={{ width: drawerWidth, CUSTOM_BG_COLOR }} role='presentation'>
+          {renderCloseButton()}
+          <Typography variant='h6' sx={{ mt: 5 }}>
+            Filters will go here for projects
+          </Typography>
+          {renderButtons()}
+
+        </Box>
+      )
+    } else if (postsView === viewFacultyFlag) {
+      return (
+        <Box sx={{ width: drawerWidth, CUSTOM_BG_COLOR }} role='presentation'>
+          {renderCloseButton()}
+          <Typography variant='h6' sx={{ mt: 5 }}>
+            Filters will go here for faculty
+          </Typography>
+          {renderButtons()}
+        </Box>
+      )
+    }
+  }
 
   return (
     <Drawer
@@ -30,32 +137,7 @@ function Sidebar ({ drawerWidth, iconColor, open, isAdmin }) {
         }
       }}
     >
-      <Box sx={{ width: drawerWidth, CUSTOM_BG_COLOR }} role='presentation'>
-        <Typography variant='h6' sx={{ mt: 5 }}>
-          Filters will go here
-        </Typography>
-
-        <List sx={{ mt: 5 }}>
-          <>
-            <ListItem disablePadding>
-              <ListItemButton onClick={() => navigate('?filter=test1')}>
-                <ListItemIcon>
-                  <SortRoundedIcon sx={{ color: iconColor }} />
-                </ListItemIcon>
-                <ListItemText primary='Test 1' />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton onClick={() => navigate('?filter=test2')}>
-                <ListItemIcon>
-                  <SortRoundedIcon sx={{ color: iconColor }} />
-                </ListItemIcon>
-                <ListItemText primary='Test 2' />
-              </ListItemButton>
-            </ListItem>
-          </>
-        </List>
-      </Box>
+      {renderDrawerContent()}
     </Drawer>
   )
 }
