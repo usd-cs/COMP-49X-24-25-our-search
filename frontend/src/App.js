7ccb086bd45ed552a93f03c95a16086c47ae0384
Sharthok <rpal@sandiego.edu>
2025-04-25 14:31:56 -0700

    Addressed Linting issues in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,21 +47,20 @@ function App () {
   // Fire these methods when the app loads
   useEffect(() => {
     if (window.Cypress) {
-      console.log("Cypress Window - Bypassing Authentication");
-      setisAuthenticated(true);
-      setIsStudent(false);
-      setIsFaculty(true);
-      setIsAdmin(false);
-      setLoading(false);
+      console.log('Cypress Window - Bypassing Authentication')
+      setisAuthenticated(true)
+      setIsStudent(false)
+      setIsFaculty(true)
+      setIsAdmin(false)
+      setLoading(false)
     } else {
-      checkAuthStatus();
+      checkAuthStatus()
     }
-  }, []);
+  }, [])
 
   // Calls the backend to check if the user is logged in
   // Sets 'isAuthenticated', 'isStudent', 'isFaculty', and 'isAdmin' variables accordingly
   const checkAuthStatus = async () => {
-
     console.log(BACKEND_URL)
     try {
       const response = await fetch(`${BACKEND_URL}/check-auth`, {
