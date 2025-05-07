e3717316c977d24fdf5f62daf192fceb3f3efa7e
Natalie Jungquist <natjungquist@gmail.com>
2025-05-07 09:12:01 -0700

    docs: added comments to the frontend fetch...() functions

--- frontend/src/utils/fetchMajors.js
+++ frontend/src/utils/fetchMajors.js
@@ -2,6 +2,8 @@
  * @file Function that calls the backend to retrieve and return a list of the majors.
  * A utility function to isolate its definition from UI-related code, and
  * because it is shared by several components.
+ * Note that a call to fetchMajors() is equivalent to a call to getDataFrom(BACKEND_ENDPOINT).
+ * The helper function getDataFrom(…) was implemented later and the earlier fetch… functions were not refactored.
  *
  * @author Natalie Jungquist <njungquist@sandiego.edu>
  */
--- frontend/src/utils/getDataFrom.js
+++ frontend/src/utils/getDataFrom.js
@@ -2,6 +2,8 @@
  * @file Function that calls the backend to retrieve and return data based on specified endpoint.
  * They are utility functions to isolate its definition from UI-related code
  * and because it is shared by several components.
+ * Note that a call to fetchDisciplines() is equivalent to a call to getDataFrom(BACKEND_ENDPOINT).
+ * The helper function getDataFrom(…) was implemented later and the earlier fetch… functions were not refactored.
  *
  * @author Natalie Jungquist <njungquist@sandiego.edu>
  */
