07702d1032a8aee1ca1351b13a696e6ec4778234
Natalie Jungquist <natjungquist@gmail.com>
2025-05-09 06:57:07 -0700

    test: added mock domain name to contextLoads()

--- backend/src/test/java/COMP_49X_our_search/backend/BackendApplicationTests.java
+++ backend/src/test/java/COMP_49X_our_search/backend/BackendApplicationTests.java
@@ -3,9 +3,13 @@ package COMP_49X_our_search.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@TestPropertySource(properties = {
+		"DOMAIN=http://localhost"
+})
 class BackendApplicationTests {
 
 	@Test
